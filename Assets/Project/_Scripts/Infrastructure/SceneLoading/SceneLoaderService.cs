28dab76b2a452be09788dba88b2f245b385cf436
edisonexe <98864739+edisonexe@users.noreply.github.com>
2026-06-19 01:04:59 +0700

    feat: Add scene loader and game navigation services

---
+++ Assets/Project/_Scripts/Infrastructure/SceneLoading/SceneLoaderService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace ArenaShooter.Infrastructure.SceneLoading
+{
+    public class SceneLoaderService
+    {
+        public void LoadScene(string sceneName, Action onSceneLoaded = null)
+        {
+            AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
+            
+            if (operation == null)
+            {
+                Debug.LogError($"[SceneLoaderService] Error load {sceneName}");
+                return;
+            }
+
+            operation.completed += _ => onSceneLoaded?.Invoke();
+        }
+    }
+}
---
+++ Assets/Project/_Scripts/Services/Gameplay/GameNavigationService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using ArenaShooter.Infrastructure.SceneLoading;
+using ArenaShooter.Infrastructure.StateMachine;
+using ArenaShooter.Infrastructure.StateMachine.States;
+
+namespace ArenaShooter.Services.Gameplay
+{
+    public class GameNavigationService
+    {
+        private readonly SceneLoaderService _sceneLoader;
+        private readonly GameStateMachine _stateMachine;
+        private readonly MatchDurationSystem _matchDurationSystem;
+        
+        private const string MENU_SCENE = "Menu";
+        private const string GAME_SCENE = "Game";
+
+        public event Action OnEnteredMenu;
+        public event Action OnEnteredGameplay;
+        
+        public GameNavigationService(SceneLoaderService sceneLoader, GameStateMachine stateMachine)
+        {
+            _sceneLoader = sceneLoader ?? throw new ArgumentNullException(nameof(sceneLoader));
+            _stateMachine = stateMachine ?? throw new ArgumentNullException(nameof(stateMachine));
+        }
+
+        public void ToGameplay()
+        {
+            _sceneLoader.LoadScene(GAME_SCENE, () =>
+            {
+                _stateMachine.TransitionTo<GameplayState>();
+                OnEnteredGameplay?.Invoke();
+            });
+        }
+
+        public void ToMainMenu()
+        {
+            _sceneLoader.LoadScene(MENU_SCENE, () =>
+            {
+                _stateMachine.TransitionTo<GameMenuState>();
+                OnEnteredMenu?.Invoke();
+            });
+        }
+    }
+}
